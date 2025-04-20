2b42fbed6c5a1ce45641bb0d53c9a1494273ffc8
Thejas Hari <thejas.hari@ei.study>
2025-04-20 20:17:24 +0530

    feedback viewer added

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -2,6 +2,7 @@
 'use client'
 
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action';
 import { cn } from '@/lib/utils';
 import { vapi } from '@/lib/vapi.sdk';
 import Image from 'next/image'
@@ -71,10 +72,11 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
         console.log(messages);
 
         // TODO: create a server action that generates feedback
-        const { success, id } = {
-            success: true,
-            id: 'feedback-id'
-        }
+        const { success, feedbackId: id } = await createFeedback({
+            interviewId: interviewId!,
+            userId: userId!,
+            transcript: messages
+        });
 
         if(success && id){
             router.push(`/interview/${interviewId}/feedback`);
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,8 +5,9 @@ import { getRandomInterviewCover } from '@/lib/utils';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
 
-const InterviewCard = ({
+const InterviewCard = async({
     id,
     userId,
     role,
@@ -14,7 +15,10 @@ const InterviewCard = ({
     techstack,
     createdAt
 }: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+    const feedback = userId && id  ? await getFeedbackByInterviewId({
+        interviewId:id,
+        userId
+    }) : null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format("MMM D, YYYY");
 
